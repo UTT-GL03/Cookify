273569f2044186b6c849a8ab5230485614850e5c
matthieu.schuler <matthieu.schuler.git@gmail.com>
2024-12-10 15:52:27 +0100

    Test avec ajout de await promise all

--- benchmarks/scenario_search.js
+++ benchmarks/scenario_search.js
@@ -4,23 +4,27 @@ const visit = async (page) => {
         waitUntil: 'networkidle',
     });
 
-    // Étape 2 : Attendre que l'input de recherche soit chargé
-    await page.waitForSelector('.search-input');
+    await Promise.all([
+        // Étape 2 : Attendre que l'input de recherche soit chargé
+        page.waitForSelector('.search-input'),
 
-    // Étape 3 : Taper un mot-clé dans la barre de recherche
-    await page.type('.search-input', 'ultrices');
+        // Étape 3 : Taper un mot-clé dans la barre de recherche
+        page.type('.search-input', 'ultrices'),
 
-    // Étape 4 : Attendre que les résultats filtrés apparaissent
-    await page.waitForTimeout(2000);
+        // Étape 4 : Attendre que les résultats filtrés apparaissent
+        page.waitForTimeout(2000),
+    ]);
 
     // Étape 5 : Vérifier la présence d'au moins un résultat
     const results = await page.$$('.recipe-card');
     if (results.length === 0) {
         throw new Error('Aucun résultat trouvé pour "Salade"');
     }
+    await Promise.all([
+        // Étape 6 : Attendre quelques secondes avant de terminer
+        page.waitForTimeout(5000)
+    ]);
 
-    // Étape 6 : Attendre quelques secondes avant de terminer
-    await page.waitForTimeout(5000);
 };
 
 module.exports = visit;
